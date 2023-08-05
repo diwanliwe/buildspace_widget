99d576a5dc0a843ab9f2b7984efada36962781a5
diwanliwe <131880908+diwanliwe@users.noreply.github.com>
2023-08-04 21:32:12 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from typing import Optional
 
 from fastapi import FastAPI
 
-app = FastAPI()
+from data import houses
 
+app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return { "gaudmire" : {"live" :{"widget_type":"person-related","author_username" : "farza", "author_ppURL" : "https://diwanliwe.github.io/sparkles_api/farza.png", "context" : "3 weeks ago", "quote_text" : "Welcome to the alternative life path","footer" : "#bsfondamentals","nameSpacer" : 6,"contextSpacer" : 4,"quoteSpacer": 25,"alignBottom" : 100,"quoteSize": 16, "background_URL" : "https://diwanliwe.github.io/sparkles_api/mountain.png","logo_URL" : "https://diwanliwe.github.io/sparkles_api/bslogo.png","content_url" : "https://twitter.com/FarzaTV/status/1678561092308705281"
+    },
+  },
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
