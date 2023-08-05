2f7931c00be2aeeeb5e022d38ac736681a60de23
diwanliwe <131880908+diwanliwe@users.noreply.github.com>
2023-08-04 21:29:46 -0700

    Create data.py

---
+++ data.py
@@ -0,0 +1,119 @@
+#Here we store all the data
+
+houses = 
+{ 
+  "gaudmire" : {
+      "house"  : {
+        "widget_type":"cta",
+        "header" : "welcome in the gaudmire house",
+        "footer" : "click on the widget to get your banner",
+        "topSpacer" : 50,
+        "alignHeader" : 18,
+        "alignBottom" : 25,
+        "background_URL" : "https://diwanliwe.github.io/sparkles_api/gaudmire.png",
+        "logo_URL" : "https://diwanliwe.github.io/sparkles_api/bslogo.png",
+        "content_url" : "https://twitter.com/FarzaTV/status/1686811120466862080"
+      },
+    "live" :{
+        "widget_type":"person-related",
+        "author_username" : "farza",
+        "author_ppURL" : "https://diwanliwe.github.io/sparkles_api/farza.png",
+        "context" : "3 weeks ago", 
+        "quote_text" : "Welcome to the alternative life path",
+        "footer" : "#bsfondamentals",
+        "nameSpacer" : 6,
+        "contextSpacer" : 4,
+        "quoteSpacer": 25,
+        "alignBottom" : 100,
+        "quoteSize": 16,
+        "background_URL" : "https://diwanliwe.github.io/sparkles_api/mountain.png",
+        "logo_URL" : "https://diwanliwe.github.io/sparkles_api/bslogo.png",
+        "content_url" : "https://twitter.com/FarzaTV/status/1678561092308705281"
+    },
+  },
+  "alterok" : {
+    "live" :{
+        "widget_type":"person-related",
+        "author_username" : "farza",
+        "author_ppURL" : "https://diwanliwe.github.io/sparkles_api/farza.png",
+        "context" : "3 weeks ago", 
+        "quote_text" : "Welcome to the alternative life path",
+        "footer" : "#bsfondamentals",
+        "nameSpacer" : 6,
+        "contextSpacer" : 4,
+        "quoteSpacer": 25,
+        "alignBottom" : 100,
+        "quoteSize": 16,
+        "background_URL" : "https://diwanliwe.github.io/sparkles_api/mountain.png",
+        "logo_URL" : "https://diwanliwe.github.io/sparkles_api/bslogo.png",
+        "content_url" : "https://twitter.com/FarzaTV/status/1678561092308705281"
+    },
+      "house"  : {
+        "widget_type":"CTA",
+        "header" : "welcome in the alterok house",
+        "topSpacer" : 50,
+        "alignHeader" : 18,
+        "alignBottom" : 18,
+        "footer" : "click on the widget to get your banner",
+        "background_URL" : "https://diwanliwe.github.io/sparkles_api/alterok.png",
+        "content_url" : "https://twitter.com/FarzaTV/status/1686811120466862080"
+      },
+    },
+  },
+  "erevald" : {
+    "live" :{
+        "widget_type":"person-related",
+        "author_username" : "farza",
+        "author_ppURL" : "https://diwanliwe.github.io/sparkles_api/farza.png",
+        "context" : "3 weeks ago", 
+        "quote_text" : "Welcome to the alternative life path",
+        "footer" : "#bsfondamentals",
+        "nameSpacer" : 6,
+        "contextSpacer" : 4,
+        "quoteSpacer": 30,
+        "alignBottom" : 100,
+        "quoteSize": 16,
+        "background_URL" : "https://diwanliwe.github.io/sparkles_api/mountain.png",
+        "logo_URL" : "https://diwanliwe.github.io/sparkles_api/bslogo.png",
+        "content_url" : "https://twitter.com/FarzaTV/status/1678561092308705281"
+    },
+      "house"  : {
+        "widget_type":"CTA",
+        "topSpacer" : 50,
+        "alignHeader" : 18,
+        "alignBottom" : 18,
+        "header" : "welcome in the erevald house",
+        "footer" : "click on the widget to get your banner",
+        "background_URL" : "https://diwanliwe.github.io/sparkles_api/erevald.png",
+        "content_url" : "https://www.youtube.com/watch?v=QXigdkdcvhs"
+      },
+  },
+  "spectreseek" : {
+    "live" :{
+        "widget_type":"person-related",
+        "author_username" : "farza",
+        "author_ppURL" : "https://diwanliwe.github.io/sparkles_api/farza.png",
+        "context" : "3 weeks ago", 
+        "quote_text" : "Welcome to the alternative life path",
+        "footer" : "#bsfondamentals",
+        "nameSpacer" : 6,
+        "contextSpacer" : 4,
+        "quoteSpacer": 25,
+        "alignBottom" : 100,
+        "quoteSize": 16,
+        "background_URL" : "https://diwanliwe.github.io/sparkles_api/mountain.png",
+        "logo_URL" : "https://diwanliwe.github.io/sparkles_api/bslogo.png",
+        "content_url" : "https://twitter.com/FarzaTV/status/1678561092308705281"
+    },
+      "house"  : {
+        "widget_type":"CTA",
+        "topSpacer" : 50,
+        "alignHeader" : 0,
+        "alignBottom" : 35,
+        "header" : "welcome in the spectreseek house",
+        "footer" : "click on the widget to get your banner",
+        "background_URL" : "https://diwanliwe.github.io/sparkles_api/spectreseek.png",
+        "content_url" : "https://www.youtube.com/watch?v=QXigdkdcvhs"
+      },
+  },
+}
