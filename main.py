e071185b3b68f378a8dcd6167b6aa20f6f4f74a9
diwanliwe <131880908+diwanliwe@users.noreply.github.com>
2023-08-07 14:56:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ async def root():
     print (houses["gaudmire"]["live"])
     return { "gaudmire" : {"live" :{"widget_type":"person-related","author_username" : "farza", "author_ppURL" : "https://diwanliwe.github.io/sparkles_api/farza.png", "context" : "3 weeks ago", "quote_text" : "Welcome to the alternative life path","footer" : "#bsfondamentals","nameSpacer" : 6,"contextSpacer" : 4,"quoteSpacer": 25,"alignBottom" : 100,"quoteSize": 16, "background_URL" : "https://diwanliwe.github.io/sparkles_api/mountain.png","logo_URL" : "https://diwanliwe.github.io/sparkles_api/bslogo.png","content_url" : "https://twitter.com/FarzaTV/status/1678561092308705281"}}}   
 
+@app.get("/gaudmire")
+async def gaudmire():
+    return {houses["gaudmire"]["live"]}
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
