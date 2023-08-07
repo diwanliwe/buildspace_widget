91eb35879f30983bf57a874487e7925de7046b19
diwanliwe <131880908+diwanliwe@users.noreply.github.com>
2023-08-07 14:51:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    print (data["gaudmire"]["live"])
+    print (houses["gaudmire"]["live"])
     return { "gaudmire" : {"live" :{"widget_type":"person-related","author_username" : "farza", "author_ppURL" : "https://diwanliwe.github.io/sparkles_api/farza.png", "context" : "3 weeks ago", "quote_text" : "Welcome to the alternative life path","footer" : "#bsfondamentals","nameSpacer" : 6,"contextSpacer" : 4,"quoteSpacer": 25,"alignBottom" : 100,"quoteSize": 16, "background_URL" : "https://diwanliwe.github.io/sparkles_api/mountain.png","logo_URL" : "https://diwanliwe.github.io/sparkles_api/bslogo.png","content_url" : "https://twitter.com/FarzaTV/status/1678561092308705281"}}}   
 
 @app.get("/items/{item_id}")
